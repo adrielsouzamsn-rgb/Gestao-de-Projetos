0bfd88d4b6081ad6ffce988bbc14b2f9cb3934a7
adrielsouzamsn-rgb <adrielsouzamsn@gmail.com>
2025-11-22 19:24:37 -0300

    Update db.php

--- backend/db.php
+++ backend/db.php
@@ -1,16 +1,15 @@
 <?php
 
-
-$host = "localhost";          // ajustar host
-$user = "root";               // usuário do banco
-$pass = "";                   // senha
-$dbname = "ppa";              // nome final do banco
+$host = "localhost";    // ajustar host
+$user = "root";         // usuario do banco  
+$pass = "";             // senha
+$dbname = "ppa";        // nome final do banco
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} 
-catch (PDOException $e) {
+    echo "Conexão bem-sucedida!";
+} catch (PDOException $e) {
     die("Erro na conexão: " . $e->getMessage());
 }
 ?>
